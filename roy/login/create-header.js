317d2cf0858d5e782929dc442099081554dc194e
Roy Garcia <roy95garcia@gmail.com>
2019-01-29 18:07:57 -0800

    practice

--- roy/login/create-header.js
+++ roy/login/create-header.js
@@ -1,13 +1,24 @@
-const header = document.createElement( tagname: 'div');
-const button = document.createElement( tagname: 'button');
-button.innertext = 'login';
-header.appendChild(button);
-const headerText = document.createtextNode( data:'hello world');
+// const header = document.createElement( tagname: 'div');
+// const button = document.createElement( tagname: 'button');
+// button.innertext = 'login';
+// header.appendChild(button);
+// const headerText = document.createtextNode( data:'hello world');
+// header.appendChild(headerText);
+// document.body.appendChild(header);
+// console.log(header);
+//
+// const headerstyle = {
+//     fontSize: '24px',
+//     color: 'blue',
+// }
+
+const header = document.createElement('div');
+const loginButton = document.createElement('button');
+loginButton.innerText = 'Login';
+header.appendChild(loginButton);
+const headerText = document.createTextNode('Hello World!');
 header.appendChild(headerText);
 document.body.appendChild(header);
 console.log(header);
 
-const headerstyle = {
-    fontSize: '24px',
-    color: 'blue',
-}
+console.log(header.style);
